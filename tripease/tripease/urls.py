a22fa9b5a48cb0b4b9910d64f80c3e65d4ab631b
AFLAH-SkyBook <aflah.skybook@gmail.com>
2024-03-07 02:17:32 +0530

    grouping

--- tripease/tripease/urls.py
+++ tripease/tripease/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .views import landing_page, hotel_register, restaurant_register, taxi_register
 
 urlpatterns = [
+    path('', landing_page, name='landing-page'),
     path('admin/', admin.site.urls),
+    path('hotel/', include('hotel.urls', namespace='hotel')),
+    path('restaurant/', include('restaurant.urls', namespace='restaurant')),
+    path('taxi/', include('taxi.urls', namespace='taxi')),
+    path('auth/register/hotel/', hotel_register, name='hotel-register'),
+    path('auth/register/restaurant/', restaurant_register, name='restaurant-register'),
+    path('auth/register/taxi/', taxi_register, name='taxi-register'),
 ]
---
+++ tripease/tripease/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+
+def hotel_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            form = form.save()
+            group = Group.objects.get(name="hotel")
+            form.groups.add(group)
+            form.save()
+            new_user = authenticate(username=username,
+                                    password=password,
+                                    )
+            login(request, new_user)
+
+        return redirect('landing-page')
+    
+    form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/hotel_register.html', context)
+
+def restaurant_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            form = form.save()
+            group = Group.objects.get(name="restaurant")
+            form.groups.add(group)
+            form.save()
+            new_user = authenticate(username=username,
+                                    password=password,
+                                    )
+            login(request, new_user)
+
+        return redirect('landing-page')
+    
+    form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/restaurant_register.html', context)
+
+def taxi_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            form = form.save()
+            group = Group.objects.get(name="taxi")
+            form.groups.add(group)
+            form.save()
+            new_user = authenticate(username=username,
+                                    password=password,
+                                    )
+            login(request, new_user)
+
+        return redirect('landing-page')
+    
+    form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/taxi_register.html', context)
+
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
