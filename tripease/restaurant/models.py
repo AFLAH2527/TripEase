fbc269a78cb334ee78c7b04e6b171d1dd3dfa312
AFLAH-SkyBook <aflah.skybook@gmail.com>
2024-03-07 01:24:42 +0530

    add to github

---
+++ tripease/restaurant/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Type(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Restaurant(models.Model):
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=50)
+    place = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
---
+++ tripease/taxi/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Type, Taxi
+
+admin.site.register(Type)
+admin.site.register(Taxi)
